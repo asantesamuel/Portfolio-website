265eeb61d456d8a14461dcff434669074e8964dd
asantesamuel <asanteboakyesamuel@gmail.com>
2024-10-01 16:14:18 +0000

    removed a lot of things
    back to default

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
-// import "./App.css";
-// import Experience from "./component/Experience";
-// import Home from "./component/Home";
-// import { Routes, Route } from "react-router-dom";
-// import Navbar from "./component/Navbar";
-// import AboutMePage from "./component/AboutMePage";
-// import ContactPage from "./component/ContactPage";
-// import Blog from "./component/Blog";
-// // import Footer from "./components/Footer";
+ import "./App.css";
+ import Experience from "./component/Experience";
+ import Home from "./component/Home";
+ import { Routes, Route } from "react-router-dom";
+ import Navbar from "./component/Navbar";
+ import AboutMePage from "./component/AboutMePage";
+ import ContactPage from "./component/ContactPage";
+ import Blog from "./component/Blog";
 
-// function App() {
-//   //no error detected
-//   return (
-//     <>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about-me" element={<AboutMePage />} />
-//         <Route path="/experience" element={<Experience />} />
-//         <Route path="/contact-me" element={<ContactPage />} />
-//         <Route path="/blog" element={<Blog />} />
-//       </Routes>
-//     </>
-//   );
-// }
-
-// export default App;
-
-import { RouterProvider } from "react-router-dom";
-
-// project import
-import router from "routes";
-import ThemeCustomization from "themes";
-
-import ScrollTop from "components/ScrollTop";
-
-// ==============================|| APP - THEME, ROUTER, LOCAL ||============================== //
-
-export default function App() {
-  return (
-    <ThemeCustomization>
-      <ScrollTop>
-        <RouterProvider router={router} />
-      </ScrollTop>
-    </ThemeCustomization>
-  );
+ function App() {
+   //no error detected
+   return (
+     <>
+       <Navbar />
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/about-me" element={<AboutMePage />} />
+         <Route path="/experience" element={<Experience />} />
+         <Route path="/contact-me" element={<ContactPage />} />
+         <Route path="/blog" element={<Blog />} />
+       </Routes>
+  </>
+   );
 }
+
+ export default App;
